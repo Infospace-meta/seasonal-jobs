a6a704a3e7957b2dcd37072aad21c91b3660d25e
joe052 <joekaranjasenior52@gmail.com>
2024-04-21 20:10:45 +0300

    message

--- app/src/views/index.js
+++ app/src/views/index.js
@@ -2,6 +2,5 @@ import HomeView from "./HomeView.vue";
 import DetailsView from "./DetailsView.vue";
 import LoginView from "./LoginView.vue";
 import PostsView from "./PostsView.vue";
-import PiniaTest from "./PiniaTest.vue";
 
-export { HomeView, DetailsView, LoginView, PostsView, PiniaTest };
+export { HomeView, DetailsView, LoginView, PostsView };
