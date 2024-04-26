5213448aaeee66475f8b6898b76cc99bf371b76f
rogers000 <ochiengrogrers.com>
2024-04-26 14:17:25 +0300

    message

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="min-h-screen bg-white">
     <Header />
-    
-    <div class="mt-4">
+    <div class="mt-14 lg:mt-28">
       <RouterView />
     </div>
     <Footer />
   </div>
-
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
-
 </script>
 
 <style scoped></style>
