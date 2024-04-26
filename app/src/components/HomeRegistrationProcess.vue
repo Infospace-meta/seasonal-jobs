86e52080df34107dc38c90f9a1fcadbd79f866b7
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-04-26 19:37:35 +0300

    edit component

--- app/src/components/HomeRegistrationProcess.vue
+++ app/src/components/HomeRegistrationProcess.vue
@@ -1,20 +1,21 @@
 <template>
   <section class="px-4 py-8 sm:py-20">
-    <div class="max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
+    <div class="max-w-5xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8">
       <!-- Text -->
-      <div class="">
-        <h1 class="font-semibold text-5xl mb-5">Registration Process</h1>
+      <div class="flex flex-col justify-center items-center md:px-0">
+        <h1 class="font-semibold text-4xl md:text-5xl mb-5">Registration Process</h1>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
           aspernatur doloribus sed repudiandae labore. Quo similique, quis quia
-          aspernatur quas sequi unde error qui incidunt reiciendis enim iure
-          tempore nam?
+          aspernatur quas sequi unde qu
         </p>
       </div>
       <!-- End Text -->
 
       <!-- Timeline: Register,Apply,Receive,Travel,Work,BringItHome -->
-      <div></div>
+      <div>
+        <!-- <p>content</p> -->
+      </div>
       <!-- End Timeline -->
     </div>
   </section>
