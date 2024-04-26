e92f88a3d85ff17cf35628bcda6364d9273bf462
billClinton048 <clintonkaranja46@gmail.com>
2024-04-26 16:11:17 +0300

    message

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="text-red-700">Home</div>
+  <AboutHeader />
 </template>
 
-<script setup></script>
+
+<script setup>
+import AboutHeader from '../components/AboutHeader.vue';
+</script>
