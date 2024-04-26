5560e474a77460361860ef14cc3047c3271cf078
meccah <georgekimiti79@gmail.com>
2024-04-26 18:33:15 +0300

    message

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -2,10 +2,12 @@
   <div>
     <Hero />
     <Cards/>
+    <Abouthome/>
   </div>
 </template>
 
 <script setup>
 import Hero from "../components/Hero.vue";
 import Cards from "../components/Cards.vue";
+import Abouthome from "../components/abouthome.vue";
 </script>
