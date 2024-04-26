8f4d7485a19cad3c4d3669822a41c981dff9b151
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-04-26 19:38:39 +0300

    edit component

--- app/src/components/HomeRegistrationProcess.vue
+++ app/src/components/HomeRegistrationProcess.vue
@@ -2,7 +2,7 @@
   <section class="px-4 py-8 sm:py-20">
     <div class="max-w-5xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-8">
       <!-- Text -->
-      <div class="flex flex-col justify-center items-center md:px-0">
+      <div class="flex flex-col justify-center md:px-0">
         <h1 class="font-semibold text-4xl md:text-5xl mb-5">Registration Process</h1>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
