78c62e58dd95a1c8cf36d5dbc5268aed5088f7a7
meccah <georgekimiti79@gmail.com>
2024-04-26 15:18:56 +0300

    message

--- app/src/components/Footer.vue
+++ app/src/components/Footer.vue
@@ -103,20 +103,23 @@
           <p>Terms of Use</p>
         </div>
         <!-- bottom_right part -->
-        <div class="flex justify-center items-center gap-2 text-gray-500">
-          <svg
+        <div class="flex justify-end items-end gap-2 text-gray-500 tracking-tight lg:tracking-normal ">
+          <div>
+            <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
+            width=""
+            height=""
             fill="currentColor"
-            class="bi bi-c-circle"
+            class="bi bi-c-circle h-3 w-3 mb-1"
             viewBox="0 0 16 16"
           >
             <path
               d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.146 4.992c-1.212 0-1.927.92-1.927 2.502v1.06c0 1.571.703 2.462 1.927 2.462.979 0 1.641-.586 1.729-1.418h1.295v.093c-.1 1.448-1.354 2.467-3.03 2.467-2.091 0-3.269-1.336-3.269-3.603V7.482c0-2.261 1.201-3.638 3.27-3.638 1.681 0 2.935 1.054 3.029 2.572v.088H9.875c-.088-.879-.768-1.512-1.729-1.512"
             />
           </svg>
-          <p>Copyright 2024. All Rights Reserved by Seasonal Jobs</p>
+          <p class="">Copyright 2024. All Rights Reserved</p>
+          </div>
+          <p>Created by InfoSpace</p>
         </div>
       </div>
     </div>
