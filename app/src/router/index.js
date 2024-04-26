55aa5b196af81131bcfbab339c40e50bd12bd9dc
rogers000 <ochiengrogrers.com>
2024-04-26 09:31:15 +0300

    message

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,12 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
-import { HomeView, PostsView, DetailsView, LoginView } from "../views";
-import { useAuthStore } from "../store";
+import { HomeView, } from "../views";
+// import { useAuthStore } from "../store";
 
 const routes = [
   { path: "/", name: "home", component: HomeView },
-  { path: "/login", name: "login", component: LoginView },
-  { path: "/posts", name: "posts", component: PostsView },
-  { path: "/posts/:id", name: "details", component: DetailsView, props: true },
 ];
 
 /**Initialize here */
--- app/src/views/index.js
+++ app/src/views/index.js
@@ -1,6 +1,3 @@
 import HomeView from "./HomeView.vue";
-import DetailsView from "./DetailsView.vue";
-import LoginView from "./LoginView.vue";
-import PostsView from "./PostsView.vue";
 
-export { HomeView, DetailsView, LoginView, PostsView };
+export { HomeView, };
