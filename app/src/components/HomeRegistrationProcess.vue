59b072da64e574b52bfd6bb18042f63fc8597e8c
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-04-26 19:25:02 +0300

    rename component

---
+++ app/src/components/HomeRegistrationProcess.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="px-4 py-8 sm:py-20">
+    <div class="max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8">
+      <!-- Text -->
+      <div class="">
+        <h1 class="font-semibold text-5xl mb-5">Registration Process</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime
+          aspernatur doloribus sed repudiandae labore. Quo similique, quis quia
+          aspernatur quas sequi unde error qui incidunt reiciendis enim iure
+          tempore nam?
+        </p>
+      </div>
+      <!-- End Text -->
+
+      <!-- Timeline: Register,Apply,Receive,Travel,Work,BringItHome -->
+      <div></div>
+      <!-- End Timeline -->
+    </div>
+  </section>
+</template>
+
+<script setup></script>
