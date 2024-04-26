55f2395d96a9d01df2dc6306db759cb17a1b1cc6
meccah <georgekimiti79@gmail.com>
2024-04-26 16:49:02 +0300

    message

--- app/src/components/Footer.vue
+++ app/src/components/Footer.vue
@@ -3,16 +3,21 @@
     <div class="max-w-7xl mx-auto py-5">
       <!-- <div class="flex flex-col justify-center items-center space-y-10 py-10 sm:flex sm:flex-row sm:justify-between sm:items-center sm:py-14 mx-5"> -->
       <div
-        class="grid grid-cols-1 justify-center items-center space-y-10 py-10 md:grid md:grid-cols-3 md:justify-between sm:items-center sm:py-14 mx-5"
+        class="grid grid-cols-1 justify-center items-center space-y-10 py-10 md:grid md:grid-cols-3 md:justify-between sm:items-center sm:py-14 mx-1"
       >
         <!-- seasonaljobs logo -->
-        <div class="ml-24 sm:ml-0">
-          <router-link to="#">
+        <div class="ml-24 sm:ml-0 flex justify-start items-start">
+          <router-link to="#" class="flex justify-center items-center">
             <img
               src="../assets/kenya-removebg.png"
               alt="seasonaljobs"
-              class="h-1/2 w-1/2"
+              class="h-1/3 w-1/3"
             />
+            <div class="flex justify-center items-center space-x-2 font-serif">
+              <p class="text-2xl text-red-600 font-bold">"Bring</p>
+              <p class="text-2xl font-bold">it</p>
+              <p class="text-2xl text-green-600 font-bold">Home!"</p>
+            </div>
           </router-link>
         </div>
         <!-- names of sections -->
