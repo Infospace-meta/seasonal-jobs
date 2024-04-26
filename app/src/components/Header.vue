69fb5a12e608d3619dfe839ce04e603d2929b68c
rogers000 <ochiengrogrers.com>
2024-04-26 12:00:06 +0300

    message

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="h-14 lg:h-28 fixed w-full top-0 bg-white z-20">
-    <div class="grid grid-cols-8 lg:grid-cols-10 h-full">
+    <div class="grid grid-cols-8 lg:grid-cols-10 mx-auto max-w-7xl h-full">
       <div
         class="col-span-5 lg:col-span-3 flex justify-center items-center gap-6"
       >
@@ -17,7 +17,7 @@
       <nav
         :class="{ hidden: close, 'sm:flex': open }"
         id="nav-content"
-        class="container bg-white col-span-4 top-0 left-0 z-20 absolute lg:relative max-lg:flex-col lg:flex lg:justify-center lg:items-center max-lg:h-screen max-lg:w-4/5 max-w-6xl max-lg:border max-lg:shadow-2xl"
+        class="container bg-white col-span-4 max-lg:top-14 left-0 z-20 absolute lg:relative max-lg:flex-col lg:flex lg:justify-center lg:items-end max-lg:h-screen  max-w-6xl max-lg:border-r max-lg:shadow-2xl"
       >
         <div class="flex flex-row-reverse justify-between lg:hidden">
           <h1
@@ -74,7 +74,24 @@
         </router-link>
       </nav>
       <div class="col-span-3 lg:col-span-3 flex justify-center items-center">
-        <img class="md:h-20 h-12" src="../assets/images/youth_logo.png" />
+        <img
+          class="max-lg:hidden md:h-20 h-12"
+          src="../assets/images/youth_logo.png"
+        />
+        <router-link
+          :to="{ name: 'home' }"
+          active-class="active-link"
+          class="lg:hidden text-gray-700 font-medium hover:text-cyan-600"
+          @click="toggleMenu"
+          ><div
+            class="inline-flex justify-center max-lg:w-full lg:p-5 pt-5 rounded-md"
+          >
+            <span
+              class="font-medium text-white bg-green-700 py-1 px-3 rounded-2xl"
+              >Register</span
+            >
+          </div>
+        </router-link>
       </div>
     </div>
   </header>
