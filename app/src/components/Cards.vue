bf32ebd53b7869469681917e78f628ad29fe52e6
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-04-26 18:29:07 +0300

    card section

---
+++ app/src/components/Cards.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="py-8 sm:py-20">
+    <div class="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4">
+      <!-- Cards -->
+      
+    </div>
+  </section>
+</template>
+
+<script setup></script>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <Hero />
+    <Cards/>
   </div>
 </template>
 
 <script setup>
 import Hero from "../components/Hero.vue";
+import Cards from "../components/Cards.vue";
 </script>
