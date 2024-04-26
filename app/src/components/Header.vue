3e2cd7599bdb88401bb7808b9c1de93cad095ddf
rogers000 <ochiengrogrers.com>
2024-04-26 10:40:03 +0300

    message

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
   <header class="h-14 lg:h-28 fixed w-full top-0 bg-white z-20">
-    <div class="grid grid-cols-8 h-full">
+    <div class="grid grid-cols-8 lg:grid-cols-10 h-full">
       <div
-        class="col-span-5 lg:col-span-2 flex justify-center items-center gap-6"
+        class="col-span-5 lg:col-span-3 flex justify-center items-center gap-6"
       >
         <h1
           class="text-2xl p-1 mt-1 text-gray-900 hover:text-indigo-700 lg:hidden"
@@ -11,13 +11,13 @@
           {{ close ? "☰" : "✘" }}
         </h1>
         <router-link to="/" class="">
-          <img class="md:h-12 h-10" src="../assets/images/seasonal_logo.png" />
+          <img class="md:h-14 h-10" src="../assets/images/seasonal_logo.png" />
         </router-link>
       </div>
       <nav
         :class="{ hidden: close, 'sm:flex': open }"
         id="nav-content"
-        class="container col-span-4 top-0 left-0 z-20 absolute lg:relative max-lg:flex-col lg:flex lg:justify-center lg:items-center max-lg:h-screen max-lg:w-4/5 max-w-6xl border shadow-2xl bg-white"
+        class="container bg-white col-span-4 top-0 left-0 z-20 absolute lg:relative max-lg:flex-col lg:flex lg:justify-center lg:items-center max-lg:h-screen max-lg:w-4/5 max-w-6xl max-lg:border max-lg:shadow-2xl "
       >
         <div class="flex flex-row-reverse justify-between lg:hidden">
           <h1
@@ -29,7 +29,7 @@
           </h1>
         </div>
       </nav>
-      <div class="col-span-3 lg:col-span-2 flex justify-center items-center gap-2">
+      <div class="col-span-3 lg:col-span-3 flex justify-center items-center">
         <img class="md:h-20 h-12" src="../assets/images/youth_logo.png" />
       </div>
     </div>
