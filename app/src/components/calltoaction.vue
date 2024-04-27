e2673b65deacbe4ee301fbc5ca7772b3c03a3792
meccah <georgekimiti79@gmail.com>
2024-04-27 12:24:37 +0300

    message

--- app/src/components/calltoaction.vue
+++ app/src/components/calltoaction.vue
@@ -1,6 +1,8 @@
 <template>
-    <section>
-        
+    <section class="py-6 lg:py-12">
+        <div class="bg-gradient-to-r from-green-600 to-gray-500 h-28 w-80 md:h-36 md:w-[600px] lg:h-48 lg:w-[1000px] mx-auto rounded-xl"> 
+          
+        </div>
     </section>
 </template>
 
