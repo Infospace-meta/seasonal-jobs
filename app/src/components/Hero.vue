a9d53078cce99f72a1a91ab6de3252a8a44be59b
rogers000 <ochiengrogrers.com>
2024-04-27 20:10:03 +0300

    message

--- app/src/components/Hero.vue
+++ app/src/components/Hero.vue
@@ -3,62 +3,58 @@
     aria-label="Site Footer"
     class="max-lg:bg-hero bg-large-hero bg-no-repeat bg-cover bg-center bg-fixed"
   >
-    <div class="opacity-90 pt-20 bg-white">
-      <div class="mx-auto max-w-6xl px-4">          
-          
-            <!-- text for small devices -->
-            <div>
-            <h1
-              class="lg:hidden text-2xl text-gray-950 font-medium tracking-tight leading-normal text-center"
+    <div class="opacity-90 lg:py-32 py-24 bg-white">
+      <div class="mx-auto max-w-6xl px-4">
+        <!-- text for small devices -->
+        <div>
+          <h1
+            class="lg:hidden text-2xl text-gray-950 font-medium tracking-tight leading-normal text-center"
+          >
+            Earn Up-to to
+            <span class="text-green-700"> KSH 80,000</span><br />($ 500 - $ 800)
+            <span class="text-green-700 font-style: italic"> per week </span
+            ><br />
+          </h1>
+          <p
+            class="lg:hidden font-light text-black text-lg text-center py-10 px-3"
+          >
+            The UK is seeking short-time labour to fill their Seasonal worker
+            scheme for 2024 in the agricultural sector
+          </p>
+        </div>
+        <div>
+          <!-- this is the text for large screens -->
+          <h1
+            class="max-lg:hidden py-7 max-w-3xl xl:max-w-7xl text-5xl xl:text-7xl text-gray-950 font-normal xl:leading-relaxed lg:leading-normal"
+          >
+            Earn Up-to to
+            <span class="text-green-700"> KSH 80,000 </span>($ 500 - $ 800)
+            <span class="text-green-700 italic"> per week </span>
+          </h1>
+          <h1
+            class="max-lg:hidden font-light text-black text-lg xl:text-2xl max-w-md xl:max-w-xl xl:translate-x-[520px] xl:-translate-y-28 lg:translate-x-[320px] lg:-translate-y-20"
+          >
+            The UK is seeking short-time labour to fill their Seasonal worker
+            scheme for 2024 in the agricultural sector
+          </h1>
+          <div class="lg:hidden flex justify-center pb-20">
+            <a
+              href="#"
+              class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-3xl bg-green-700 hover:bg-green-700"
             >
-              Earn Up-to to
-              <span class="text-green-700"> KSH 80,000</span><br />($ 500 - $
-              800)
-              <span class="text-green-700 font-style: italic"> per week </span
-              ><br />
-            </h1>
-            <p class=" lg:hidden font-light text-black text-lg text-center py-10 px-3">
-              The UK is seeking short-time labour to fill their Seasonal worker
-              scheme for 2024 in the agricultural sector</p>
-            
-            </div>
-            <div>
-            <!-- this is the text for large screens -->
-            <h1
-              class="max-lg:hidden py-7 max-w-3xl xl:max-w-7xl text-5xl xl:text-7xl text-gray-950 font-normal xl:leading-relaxed lg:leading-normal"
-            >
-              Earn Up-to to
-              <span class="text-green-700"> KSH 80,000 </span>($ 500 - $
-              800)
-              <span class="text-green-700 italic"> per week </span
-              >
-             
-            </h1>
-            <h1 class=" max-lg:hidden font-light text-black text-lg xl:text-2xl max-w-md xl:max-w-xl xl:translate-x-[520px] xl:-translate-y-28 lg:translate-x-[320px] lg:-translate-y-20">
-              The UK is seeking short-time labour to fill their Seasonal worker
-              scheme for 2024 in the agricultural sector</h1>
-            <div class="lg:hidden flex justify-center">
-              <a
-                href="#"
-                class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-3xl bg-green-700 hover:bg-green-700"
-              >
-                Register Now
-              </a>
-            </div>
+              Register Now
+            </a>
           </div>
         </div>
-      <div class="text-green-500 text-center mx-auto">
-        <a
-          href="#"
-          class="flex flex-col items-center justify-center font-medium text-green-800 "
-        >
-        <h1 class="text-center text-lg">Discover more </h1>
-        <h1 class="text-center text-4xl animate-bounce">&#9759</h1>       
-        </a>   
       </div>
+      <a
+        href="#"
+        class="flex flex-col items-center justify-center text-lg font-medium text-green-800"
+        >Discover more
+        <h1 class="text-center text-3xl">&#x261F;</h1>
+      </a>
     </div>
   </div>
-  
 </template>
 
 <script setup></script>
