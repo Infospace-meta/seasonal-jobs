4705cb0004640595609900d70b419dcafc1cfdf6
billClinton048 <clintonkaranja46@gmail.com>
2024-04-27 16:16:25 +0300

    message

--- app/src/components/AboutCards.vue
+++ app/src/components/AboutCards.vue
@@ -1,7 +1,11 @@
 <template>
 
     <section class="divide-solid divide-y py-5 max-w-6xl mx-auto">
+
+        <!-- seasonal job container -->
     <div class="w-full bg-transparent flex flex-col justify-between  py-10 items-center md:flex-row">
+        
+        <!-- Seasonal job text -->
         <div class="p-5 w-full md:w-5/12">
             <h1 class="text-2xl left mb-5 hover:underline  md:text-4xl ">Seasonal Jobs</h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
@@ -23,7 +27,10 @@
 
         </div>
         
-        <div class="w-full px-5 h-72 bg-slate-400 rounded-md md:w-5/12"></div>
+        <!-- seasonal job image -->
+        <div class="w-full px-5 h-72 rounded-md md:w-5/12">
+            <img src="../assets/Farm.png" class="w-full h-full transition hover:scale-125">
+        </div>
 
 
 
@@ -31,13 +38,18 @@
     </div>
 
 
-
+      <!--Bring it home container -->
     <div class="w-full bg-transparent flex flex-col-reverse justify-between  py-10 items-center md:flex-row">
         
-        <div class="w-full h-72 bg-slate-400 rounded-md px-5 md:w-5/12"></div>
+        <!-- bring it image  home -->
+        <div class="w-full h-72 rounded-md px-5 md:w-5/12">
 
+            <img src="../assets/kenya_wbg.png" class="w-full h-full transition hover:scale-125">
 
-        <div class="w-full p-5 md:w-5/12">
+        </div>
+        
+        <!-- bring it home contect -->
+        <div class="w-full p-5 md:w-5/12"> 
             <h1 class="text-2xl left pb-5 hover:underline  md:text-4xl ">Bring It Home</h1>
             <p class="block">Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
                 Optio fuga sint adipisci est repellat sed dicta,
@@ -58,8 +70,12 @@
         </div>
     </div>
 
-
+        <!-- Bring it home container -->
     <div class="w-full bg-transparent flex flex-col justify-between  py-10 items-center md:flex-row">
+
+
+
+        <!-- Bring it home content -->
         <div class="w-full p-5 md:w-5/12">
             <h1 class="text-2xl left mb-5 hover:underline  md:text-4xl ">Bring It Home</h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
@@ -80,21 +96,33 @@
 
 
         </div>
+
+        <!-- Bring it home image -->
         
-        <div class="w-full h-72 bg-slate-400 rounded-md md:w-5/12"></div>
+        <div class="w-full h-72  rounded-md md:w-5/12">
+            <img src="../assets/kenya_wbg.png" class="w-full h-full mx-5 transition hover:scale-125">
+        </div>
 
 
 
 
     </div>
 
-
+        <!-- bring it home Youthfund -->
     <div class="w-full bg-transparent flex flex-col-reverse justify-between  py-10 items-center md:flex-row">
         
-        <div class="w-full h-72 bg-slate-400 rounded-md md:w-5/12"></div>
+        <!-- Youthfund image -->
+        <div class="w-full h-72 rounded-md md:w-5/12">
 
 
-        <div class="w-full md:w-5/12 my-5">
+            <img src="../assets/kenya_wbg.png" class="w-full h-full mx-5 transition hover:scale-125">
+
+
+
+        </div>
+
+            <!-- Youthfund content -->
+        <div class="w-full p-5 md:w-5/12">
             <h1 class="text-2xl left mb-5 md:text-4xl hover:underline">Youthfund Partnership</h1>
             <p class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
                 Optio fuga sint adipisci est repellat sed dicta,
