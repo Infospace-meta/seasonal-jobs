380c40cc26642301295f574a7aa2a287a3b1344c
SMRnj <samaranj157@gmail.com>
2024-04-27 15:26:37 +0300

    FAQs

---
+++ app/src/components/faqs.vue
@@ -0,0 +1,138 @@
+<template>
+<section class="py-20">
+  <div class="max-w-7xl mx-auto px-4 sm:px-8">
+    <div class="py-8 px-4 mx-auto max-w-7xl text-center lg:py-8 lg:px-0">
+    <h1
+      class="mb-4 text-4xl font-semibold tracking-tight leading-none text-black md:text-5xl lg:text-6xl"
+    >
+      Frequently
+      <span
+        class="text-transparent bg-clip-text bg-gradient-to-r from-blue-100 to-red-700"
+      >
+        Asked
+      </span>
+      <span 
+        class="text-transparent bg-clip-text bg-gradient-to-r from-red-50 to-green-700">
+      Questions
+      </span>
+    </h1>
+    <p
+      class="mb-0 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48"
+    >
+        Empowering Kenyan Migrant Workers:
+        Investing in Their Future, Their Familys’
+        Future and the Nation's Prosperity
+    </p>
+  </div>
+    <div class="max-w-3xl mx-auto">
+      <ul class="mt-0 divide-y-2 rounded-2xl">
+        <li>
+          <details class="group">
+            <summary
+              class="flex items-center justify-between px-4 sm:px-8 py-3 font-medium hover:cursor-pointer"
+            >
+              <span class="font-semibold text-xl py-2">
+                How to create an account?
+              </span>
+              <span class="text-2xl">+</span>
+            </summary>
+
+            <div class="m-4 p-4 rounded-2xl">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et
+                ipsum sapien. Vestibulum molestie porttitor augue vitae
+                vulputate. Aliquam nec ex maximus, suscipit diam vel, tristique
+                tellus.
+              </p>
+            </div>
+          </details>
+        </li>
+        <li>
+            <details class="group">
+            <summary
+              class="flex items-center justify-between px-4 sm:px-8 py-3 font-medium hover:cursor-pointer"
+            >
+              <span class="font-semibold text-xl py-2">
+                Will I get an email notification?
+              </span>
+              <span class="text-2xl">+</span>
+            </summary>
+
+            <div class="m-4 p-4 rounded-2xl">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et
+                ipsum sapien. Vestibulum molestie porttitor augue vitae
+                vulputate. Aliquam nec ex maximus, suscipit diam vel, tristique
+                tellus.
+              </p>
+            </div>
+          </details>
+        </li>
+        <li>
+          <details class="group">
+            <summary
+              class="flex items-center justify-between px-4 sm:px-8 py-3 font-medium hover:cursor-pointer"
+            >
+              <span class="font-semibold text-xl py-2">
+                How can I make payment?
+              </span>
+              <span class="text-2xl">+</span>
+            </summary>
+
+            <div class="m-4 p-4 rounded-2xl">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et
+                ipsum sapien. Vestibulum molestie porttitor augue vitae
+                vulputate. Aliquam nec ex maximus, suscipit diam vel, tristique
+                tellus.
+              </p>
+            </div>
+          </details>
+        </li>
+        <li>
+          <details class="group">
+            <summary
+              class="flex items-center justify-between px-4 sm:px-8 py-3 font-medium hover:cursor-pointer"
+            >
+              <span class="font-semibold text-xl py-2">
+                Do you provide discounts?
+              </span>
+              <span class="text-2xl">+</span>
+            </summary>
+
+            <div class="m-4 p-4 rounded-2xl">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et
+                ipsum sapien. Vestibulum molestie porttitor augue vitae
+                vulputate. Aliquam nec ex maximus, suscipit diam vel, tristique
+                tellus.
+              </p>
+            </div>
+          </details>
+        </li>
+        <li>
+          <details class="group">
+            <summary
+              class="flex items-center justify-between px-4 sm:px-8 py-3 font-medium hover:cursor-pointer"
+            >
+              <span class="font-semibold text-xl py-2">
+                What am I getting as a Premium Member?
+              </span>
+              <span class="text-2xl">+</span>
+            </summary>
+
+            <div class="m-4 p-4 rounded-2xl">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et
+                ipsum sapien. Vestibulum molestie porttitor augue vitae
+                vulputate. Aliquam nec ex maximus, suscipit diam vel, tristique
+                tellus.
+              </p>
+            </div>
+          </details>
+        </li>
+      </ul>
+    </div>
+  </div>
+</section>
+</template>
--- app/src/views/ContactView.vue
+++ app/src/views/ContactView.vue
@@ -2,8 +2,12 @@
   <div>
  <contact/>
  </div>
+ <div>
+   <faqs/>
+ </div>
 </template>
 
 <script setup>
 import contact from '../components/contact.vue';
+import faqs  from '../components/faqs.vue';
 </script>
