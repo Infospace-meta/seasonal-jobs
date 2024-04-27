3cfa6d37c4ef36201684cdb1be59322a17379628
meccah <georgekimiti79@gmail.com>
2024-04-27 14:09:03 +0300

    message

--- app/src/components/calltoaction.vue
+++ app/src/components/calltoaction.vue
@@ -1,38 +1,38 @@
 <template>
   <section class="py-6 lg:py-12">
     <div
-      class="relative bg-gradient-to-r from-green-400 to-gray-700 h-28 w-[370px] md:h-36 md:w-[600px] lg:h-48 lg:w-[1000px] mx-auto rounded-xl"
+      class="relative bg-gradient-to-l from-gray-600 to-green-700  h-28 w-[370px] md:h-36 md:w-[600px] lg:h-48 lg:w-[1000px] mx-auto rounded-xl"
     >
-      <div class="absolute lg:top-5 lg:left-16">
-        <h1 class="sm:text-md lg:text-xl font-medium text-gray-600">
+      <div class="absolute sm:top-2 sm:left-8 lg:top-5 lg:left-16">
+        <h1 class="sm:text-md lg:text-xl font-medium text-white">
           Want to Work in the UK
         </h1>
       </div>
       <div
-        class="absolute lg:left-16 lg:top-16 flex justify-center items-center lg:pb-5"
+        class="absolute sm:left-8 sm:top-10 lg:left-16 lg:top-16 flex justify-center items-center lg:pb-5"
       >
         <router-link to="/register">
           <button
-            class="bg-gradient-to-l from-blue-500 to-gray-200 px-3 py-3 rounded-full text-gray-900 text-sm"
+            class="bg-orange-400  px-3 py-3 rounded-full text-gray-900 text-sm "
           >
             Register Here to get a Chance
           </button></router-link
         >
       </div>
-      <div class="absolute lg:left-16 lg:bottom-10">
-        <span class="text-gray-700"
-          >Get paid <strong>Ksh 80,000 Per Week</strong> working in a farm, then
-          <strong>Invest</strong> back home.</span
+      <div class="absolute sm:left-8 sm:bottom-2 lg:left-16 lg:bottom-7">
+        <span class="text-white"
+          >Get paid <strong>Ksh 80,000 Per Week</strong> working in a farm,<br />
+          then <strong>Invest</strong> back home.</span
         >
       </div>
       <div
         class="absolute right-0 top-5 flex justify-end items-center space-x-2 font-serif lg:px-10 lg:pt-10 sm:px-5 sm:pt-5 px-5 pt-5"
       >
-        <p class="text-md md:text-2xl lg:text-4xl text-red-600 font-bold">
+        <p class="text-md md:text-2xl lg:text-4xl text-white font-bold">
           "Bring
         </p>
-        <p class="text-md md:text-2xl lg:text-4xl font-bold">it</p>
-        <p class="text-md md:text-2xl lg:text-4xl text-green-600 font-bold">
+        <p class="text-md md:text-2xl lg:text-4xl font-bold text-white">it</p>
+        <p class="text-md md:text-2xl lg:text-4xl text-white font-bold">
           Home!"
         </p>
       </div>
