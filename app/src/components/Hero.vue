f09cd7451d91500fd2fa78c4e66dc18173019e0e
rogers000 <ochiengrogrers.com>
2024-04-27 12:17:03 +0300

    message

--- app/src/components/Hero.vue
+++ app/src/components/Hero.vue
@@ -25,7 +25,7 @@
 
             <!-- this is the text for large screens -->
             <h1
-              class="max-lg:hidden py-5 max-w-7xl text-7xl text-gray-950 font-normal leading-relaxed"
+              class="max-lg:hidden py-5 max-w-5xl lg:max-w-7xl text-5xl lg:text-7xl text-gray-950 font-normal lg:leading-relaxed"
             >
               Earn Up-to to
               <span class="text-green-700"> KSH 80,000 </span>($ 500 - $
@@ -34,7 +34,7 @@
               >
              
             </h1>
-            <h1 class=" max-lg:hidden font-light text-black text-2xl max-w-xl translate-x-[520px] -translate-y-28">
+            <h1 class=" max-lg:hidden font-light text-black text-2xl max-w-md lg:max-w-xl lg:translate-x-[520px] lg:-translate-y-28">
               The UK is seeking short-time labour to fill their Seasonal worker
               scheme for 2024 in the agricultural sector</h1>
             <div class="flex justify-center">
