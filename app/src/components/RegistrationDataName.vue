20be4d6f64271ccde7de353b9c056208e1a02c59
rogers000 <ochiengrogrers.com>
2024-04-27 16:19:45 +0300

    message

--- app/src/components/RegistrationDataName.vue
+++ app/src/components/RegistrationDataName.vue
@@ -95,6 +95,140 @@
             />
           </div>
         </div>
+        <div class="grid lg:grid-cols-3 gap-4 py-2">
+          <div class="col-span-1">
+            <p class="py-2 text-lg">Gender</p>
+            <div class="flex flex-col">
+              <div class="flex gap-3">
+                <input
+                  type="radio"
+                  id="male"
+                  name="gender"
+                  value="male"
+                  class="border border-gray-400 rounded-md"
+                />
+                <label for="male">Male</label>
+              </div>
+              <div class="flex gap-3">
+                <input
+                  type="radio"
+                  id="female"
+                  name="gender"
+                  value="female"
+                  class="border border-gray-400 rounded-md"
+                />
+                <label for="female">Female</label>
+                <!-- Add more radio buttons for other genders if needed -->
+              </div>
+            </div>
+          </div>
+          <div class="col-span-1">
+            <p class="py-2 text-lg">Age</p>
+            <div class="flex flex-col">
+              <div class="flex gap-3">
+                <input
+                  type="radio"
+                  id="18 - 38"
+                  name="age"
+                  value="18 - 38"
+                  class="border border-gray-400 rounded-md"
+                />
+                <label for="male">18 - 38</label>
+              </div>
+              <div class="flex gap-3">
+                <input
+                  type="radio"
+                  id="38 - 45"
+                  name="age"
+                  value="38 - 45"
+                  class="border border-gray-400 rounded-md"
+                />
+                <label for="38 - 45">38 - 45</label>
+                <!-- Add more radio buttons for other genders if needed -->
+              </div>
+            </div>
+          </div>
+          <div class="col-span-1">
+            <p class="py-2 text-lg">Pre-existing Medical Conditions</p>
+            <div class="flex flex-col">
+              <div class="flex gap-3">
+                <input
+                  type="radio"
+                  id="Yes"
+                  name="Medical Conditions"
+                  value="Yes"
+                  class="border border-gray-400 rounded-md"
+                />
+                <label for="Yes">Yes</label>
+              </div>
+              <div class="flex gap-3">
+                <input
+                  type="radio"
+                  id="No"
+                  name="Medical Conditions"
+                  value="No"
+                  class="border border-gray-400 rounded-md"
+                />
+                <label for="No">No</label>
+                <!-- Add more radio buttons for other genders if needed -->
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="grid lg:grid-cols-3 gap-4 py-2">
+          <div class="col-span-1">
+            <p class="py-2 text-lg">Agricultural Experience</p>
+            <div class="flex flex-col">
+              <div class="flex gap-3">
+                <input
+                  type="radio"
+                  id="Yes"
+                  name="Experience"
+                  value="Yes"
+                  class="border border-gray-400 rounded-md"
+                />
+                <label for="Yes">Yes</label>
+              </div>
+              <div class="flex gap-3">
+                <input
+                  type="radio"
+                  id="No"
+                  name="Experience"
+                  value="No"
+                  class="border border-gray-400 rounded-md"
+                />
+                <label for="No">No</label>
+                <!-- Add more radio buttons for other genders if needed -->
+              </div>
+            </div>
+          </div>
+          <div class="col-span-1">
+            <p class="py-2 text-lg">Youth Fund Loan</p>
+            <div class="flex flex-col">
+              <div class="flex gap-3">
+                <input
+                  type="radio"
+                  id="Yes"
+                  name="Loan"
+                  value="Yes"
+                  class="border border-gray-400 rounded-md"
+                />
+                <label for="Yes">Yes</label>
+              </div>
+              <div class="flex gap-3">
+                <input
+                  type="radio"
+                  id="No"
+                  name="Loan"
+                  value="No"
+                  class="border border-gray-400 rounded-md"
+                />
+                <label for="No">No</label>
+                <!-- Add more radio buttons for other genders if needed -->
+              </div>
+            </div>
+          </div>
+        </div>
 
         <!-- <input
           v-model="formData.firstName"
@@ -123,7 +257,7 @@
     </div>
     <div class="flex justify-end py-5">
       <button
-        class="text-white bg-green-600 px-4 py-2 rounded-md"
+        class="text-white bg-green-700 px-4 py-2 rounded-md"
         @click="finishForm"
       >
         Accept
--- app/src/views/RegisterView.vue
+++ app/src/views/RegisterView.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="relative">
-    <div
-      class="bg-large-hero bg-no-repeat bg-cover bg-center bg-fixed h-screen"
-    >
-      <div class="inset-0 bg-white opacity-90 h-full"></div>
+    <div class="bg-large-hero bg-no-repeat bg-cover bg-center bg-fixed">
+      <div
+        class="inset-0 bg-white opacity-90"
+        style="height: calc(100vh + 100px)"
+      ></div>
     </div>
     <div
       class="absolute z-10 top-0 px-2 flex justify-center w-full py-10 items-center"
