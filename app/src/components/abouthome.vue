223b78575190b189f08ee63ac4f4b80c27e978b5
meccah <georgekimiti79@gmail.com>
2024-04-30 08:30:17 +0300

    message

--- app/src/components/abouthome.vue
+++ app/src/components/abouthome.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="mx-auto max-w-5xl">
+  <section class="mx-auto max-w-5xl py-10">
     <div class="flex justify-center items-center space-x-2 font-serif">
       <p class="text-2xl text-red-600 font-bold">"Bring</p>
       <p class="text-2xl font-bold">it</p>
@@ -11,13 +11,13 @@
         class="grid grid-cols-1 justify-center items-center md:grid md:grid-cols-1 md:justify-center md:items-center lg:grid lg:grid-cols-2 lg:justify-center lg:items-center"
       >
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-          laudantium cumque inventore sequi sed! Iusto.
+          The UK is seeking short-term labour ti fill their seasonal worker
+          scheme in 2024 in the Agriculture sector.
         </p>
 
-        <div>
+        <!-- <div>
           <img src="../assets/kenya_wbg.png" alt="seasonaljobs" class="" />
-        </div>
+        </div> -->
       </div>
     </div>
   </section>
