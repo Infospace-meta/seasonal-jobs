98e0ddc4156f3c740ca4c70f15b8dd89144ee94e
meccah <georgekimiti79@gmail.com>
2024-05-07 19:25:20 +0300

    message

--- app/src/components/calltoaction.vue
+++ app/src/components/calltoaction.vue
@@ -1,13 +1,16 @@
 <template>
-    <section>
-        
-    </section>
+  <section class="mx-auto max-w-5xl h-32 w-full">
+    <div class="bg-gradient-to-r from-green-600 to-gray-400">
+      <div></div>
+      <button class="bg-green-600 px-4 rounded-full flex justify-end items-end">
+        <router-link to="/register"
+          >Press me to get the opportunity</router-link
+        >
+      </button>
+    </div>
+  </section>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
