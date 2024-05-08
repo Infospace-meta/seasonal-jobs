862d1f289cd36639f710e01360a1395533850c2c
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-05-09 00:58:13 +0300

    update footer

--- app/src/components/Hero.vue
+++ app/src/components/Hero.vue
@@ -1,33 +1,30 @@
 <template>
- <div class="relative overflow-hidden">
-  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
-    <div class="max-w-2xl text-center mx-auto mt-10">
-      <h1
-        class="font-semibold text-gray-800 sm:text-6xl md:text-6xl text-lg"
-      >
-        Earn Up to KSH 80,000 ($500 - $800) <span class="text-green-600">per week</span>
-      </h1>
-      <p class="mt-8 text-lg text-gray-800 ">
-         The UK is seeking short-time labour to fill their Seasonal worker
+  <div class="relative overflow-hidden">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
+      <div class="max-w-2xl text-center mx-auto mt-10">
+        <h1 class="font-semibold text-gray-800 sm:text-6xl md:text-6xl text-3xl">
+          Earn Up to KSH 80,000 ($500 - $800)
+          <span class="text-green-600">per week</span>
+        </h1>
+        <p class="mt-8 text-sm sm:text-lg text-gray-800">
+          The UK is seeking short-time labour to fill their Seasonal worker
           scheme for 2024 in the agricultural sector
-      </p>
+        </p>
+      </div>
     </div>
   </div>
- </div>
- <div class=" flex justify-center pb-30">
-          <a
-            href="#"
-            class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-3xl bg-green-700 hover:bg-green-700"
-          >
-            Register Now
-          </a>
-        </div>
-   
+  <div class="flex justify-center pb-24">
+    <a
+      href="#"
+      class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-3xl bg-green-700 hover:bg-green-800"
+    >
+      Register Now
+    </a>
+  </div>
 </template>
 
 <script setup>
-  // No script setup code yet
+// No script setup code yet
 </script>
 
 <!-- No custom styles defined -->
-
