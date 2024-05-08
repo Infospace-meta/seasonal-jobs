afefe3d212b10f31e0558b05b8855a290223c98e
Nellit <kamaunelson157@gmail.com>
2024-05-09 00:25:49 +0300

    message

--- app/src/components/Hero.vue
+++ app/src/components/Hero.vue
@@ -1,42 +1,20 @@
 <template>
-  <div class="lg:py-32 py-24 bg-white">
-    <div class="mx-auto max-w-6xl px-4">
-      <!-- text for small devices -->
-      <div>
-        <h1
-          class="lg:hidden text-2xl text-gray-950 font-medium tracking-tight leading-normal text-center"
-        >
-          Earn Up to
-          <span class="text-green-700"> KSH 80,000</span><br />($500 - $800)
-          <span class="text-green-700 font-style: italic"> per week </span
-          ><br />
-        </h1>
-        <p
-          class="lg:hidden font-light text-black text-lg text-center py-10 px-3"
-        >
-          The UK is seeking short-time labour to fill their Seasonal worker
+ <div class="relative overflow-hidden">
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
+    <div class="max-w-2xl text-center mx-auto mt-10">
+      <h1
+        class="font-semibold text-gray-800 sm:text-6xl md:text-6xl text-lg"
+      >
+        Earn Up to KSH 80,000 ($500 - $800) <span class="text-green-600">per week</span>
+      </h1>
+      <p class="mt-8 text-lg text-gray-800 ">
+         The UK is seeking short-time labour to fill their Seasonal worker
           scheme for 2024 in the agricultural sector
-        </p>
-      </div>
-      <div>
-        <!-- this is the text for large screens -->
-        <h1
-          class="max-lg:hidden py-7 mx-auto max-w-3xl xl:max-w-7xl text-5xl xl:text-7xl text-gray-950 font-normal xl:leading-relaxed lg:leading-normal"
-        >
-          Earn Up to
-          <span class="text-transparent bg-clip-text bg-gradient-to-r from-red-300 to-red-700"> KSH 80,000 </span>
-          <span class="text-transparent bg-clip-text bg-gradient-to-r from-red-300 to-red-700">
-          ($500 - $800)
-          </span>
-          <span class="text-green-700 italic text-transparent bg-clip-text bg-gradient-to-r from-green-300 to-green-700"> per week </span>
-        </h1>
-        <h1
-          class="max-lg:hidden font-light text-black text-lg xl:text-2xl max-w-md xl:max-w-xl xl:translate-x-[520px] xl:-translate-y-28 lg:translate-x-[520px] lg:-translate-y-20"
-        >
-          The UK is seeking short-time labour to fill their Seasonal worker
-          scheme for 2024 in the agricultural sector
-        </h1>
-        <div class="lg:hidden flex justify-center pb-20">
+      </p>
+    </div>
+  </div>
+ </div>
+ <div class=" flex justify-center pb-30">
           <a
             href="#"
             class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-3xl bg-green-700 hover:bg-green-700"
@@ -44,17 +22,12 @@
             Register Now
           </a>
         </div>
-      </div>
-    </div>
-    <a
-      href="#"
-      class="flex flex-col items-center justify-center text-lg font-medium text-green-800"
-      >Discover more
-      <h1 class="text-center text-3xl">&#x261F;</h1>
-    </a>
-  </div>
+   
 </template>
 
-<script setup></script>
+<script setup>
+  // No script setup code yet
+</script>
 
-<style lang="scss" scoped></style>
+<!-- No custom styles defined -->
+
