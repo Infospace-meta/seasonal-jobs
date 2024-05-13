a03b92765e39dc45d6d634500e02fef63fae62a0
meccah <georgekimiti79@gmail.com>
2024-05-13 18:00:32 +0300

    message

--- app/src/components/Rememberthis.vue
+++ app/src/components/Rememberthis.vue
@@ -1,45 +1,58 @@
 <template>
-  <section class="mx-auto max-w-5xl ">
-    <div class="mx-5">
-    <div class="border-[3px] border-green-600 rounded-3xl px-10 py-5">
-      <h1 class="px-11 py-2 font-semibold">Remember.</h1>
-      <div class="flex justify-start items-start">
-        <img
-          src="../assets/icons/arrow-right-svgrepo-com.svg"
-          class="h-12 w-12 pb-5"
-          alt="arrow"
-        />
-        <p class="place-self-start">Increase in wages per hour in 2024</p>
+  <section class="mx-auto max-w-6xl">
+    <div
+      class="mx-5 grid grid-cols-1 sm:grid sm:grid-cols-2 gap-5 border-[3px] border-green-600 rounded-3xl px-10 py-5"
+    >
+      <div>
+        <h1 class="px-11 py-2 font-semibold">Remember.</h1>
+        <div class="flex justify-start items-start">
+          <img
+            src="../assets/icons/arrow-right-svgrepo-com.svg"
+            class="h-12 w-12 pb-5"
+            alt="arrow"
+          />
+          <p class="place-self-start">Increase in wages per hour in 2024</p>
+        </div>
+        <div class="flex justify-start items-start">
+          <img
+            src="../assets/icons/arrow-right-svgrepo-com.svg"
+            class="h-12 w-12 pb-5"
+            alt="arrow"
+          />
+          <p>Machinery skills added advantage</p>
+        </div>
+        <div class="flex justify-start items-start">
+          <img
+            src="../assets/icons/arrow-right-svgrepo-com.svg"
+            class="h-12 w-12 pb-5"
+            alt="arrow"
+          />
+          <p>Accommodation at government rate</p>
+        </div>
+        <div class="flex justify-start items-start">
+          <img
+            src="../assets/icons/arrow-right-svgrepo-com.svg"
+            class="h-12 w-12 pb-5"
+            alt="arrow"
+          />
+          <p>Only passport and flight ticket required</p>
+        </div>
       </div>
-      <div class="flex justify-start items-start">
-        <img
-          src="../assets/icons/arrow-right-svgrepo-com.svg"
-          class="h-12 w-12 pb-5"
-          alt="arrow"
-        />
-        <p>Machinery skills added advantage</p>
-      </div>
-      <div class="flex justify-start items-start">
-        <img
-          src="../assets/icons/arrow-right-svgrepo-com.svg"
-          class="h-12 w-12 pb-5"
-          alt="arrow"
-        />
-        <p>Accommodation at government rate</p>
-      </div>
-      <div class="flex justify-start items-start">
-        <img
-          src="../assets/icons/arrow-right-svgrepo-com.svg"
-          class="h-12 w-12 pb-5"
-          alt="arrow"
-        />
-        <p>Only passport and flight ticket required</p>
-      </div>
-    </div>
-    <div>
+      <div class="space-y-10">
+        <h1 class="py-10">
+          Want to Earn Yourself some Money Working in the UK
+        </h1>
 
+        <router-link
+          to="/register"
+          class="px-4 py-2 bg-orange-500 rounded-full"
+        >
+          <button class="text-gray-600">Register</button>
+        </router-link>
+
+        <p>Seasonal jobs is here for you</p>
+      </div>
     </div>
-  </div>
   </section>
 </template>
 
