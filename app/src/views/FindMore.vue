1e962407d866f2b6b9214fa55c71d4b4cde70c4f
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-05-14 23:57:51 +0300

    message

--- app/src/views/FindMore.vue
+++ app/src/views/FindMore.vue
@@ -1,13 +1,7 @@
 <template>
-    <div>
-
-    </div>
+  <div class="bg-white"></div>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
