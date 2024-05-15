a02329b0e419c0a65704fec5a21c89c1da0198fa
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-05-15 04:25:42 +0300

    message

--- app/src/views/FindMore.vue
+++ app/src/views/FindMore.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="bg-white"></div>
+  <div class="bg-white bg-opacity-95">
+    <FindMoreComponent/>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import FindMoreComponent from "../components/FindMoreComponent.vue"
+</script>
 
 <style lang="scss" scoped></style>
