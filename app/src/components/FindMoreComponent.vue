a54c9bc1f4485c8d852af8d7e23957df59c29a21
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-05-15 13:16:38 +0300

    message

--- app/src/components/FindMoreComponent.vue
+++ app/src/components/FindMoreComponent.vue
@@ -1,53 +1,136 @@
 <template>
   <!-- Visit and Travel -->
-  <section class="py-20 max-w-6xl mx-auto">
+  <section class="py-10 max-w-6xl mx-auto">
     <div class="mx-auto px-4 sm:px-8">
       <div class="py-8 px-4 mx-auto max-w-4xl text-center lg:py-8 lg:px-0">
         <h1
-          class="mb-4 text-2xl font-semibold tracking-tight leading-none text-black md:text-3xl lg:text-4xl"
+          class="mb-4 text-3xl font-semibold tracking-tight leading-none text-black md:text-4xl lg:text-5xl"
         >
           Traveling to the UK
         </h1>
-        <p class="mb-0 text-sm font-normal text-gray-900 lg:text-xl sm:px-16">
-          After application what next? <br/> 
-          It is the responsibility of the applicant
-          to pay for the Biometric data collection & the Seasonal Job Visa which
-          will be approved with a Sponsorship Number from the UK. then obtain a
-          passport, a letter of Good Conduct (confirm necessity), Visa with
-          biometrics and, finally, with the assistance of Seasonal Jobs an air
-          ticket for travel.
+        <p class="mb-0 text-lg font-normal text-gray-900 lg:text-xl sm:px-16">
+          After application what next? <br />
+          It is the responsibility of the applicant to pay for the Biometric
+          data collection & the Seasonal Job Visa which will be approved with a
+          Sponsorship Number from the UK. then obtain a passport, a letter of
+          Good Conduct (confirm necessity), Visa with biometrics and, finally,
+          with the assistance of Seasonal Jobs an air ticket for travel.
         </p>
       </div>
     </div>
     <!-- Images -->
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 py-8 px-4">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 py-8 px-4">
       <!-- Image 1 -->
-      <img
+      <!-- <img
         class="w-full h-52 rounded-2xl"
         src="https://images.unsplash.com/photo-1550788919-a18fe7099302?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YnJpdGFpbnxlbnwwfHwwfHx8MA%3D%3D"
         alt="Image Description"
-      />
+      /> -->
+      <div
+        class="w-full h-52 rounded-3xl bg-gray-100 "
+      ></div>
       <!-- Image 2 -->
-      <img
-        class="w-full h-52 rounded-2xl"
-        src="https://images.unsplash.com/photo-1444858291040-58f756a3bdd6?q=80&w=1978&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Image Description"
-      />
+      <div
+        class="w-full h-52 rounded-3xl bg-gray-100 "
+      ></div>
       <!-- Image 3 -->
-
-      <img
-        class="w-full h-52 rounded-2xl"
-        src="https://images.unsplash.com/photo-1436491865332-7a61a109cc05?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="Image Description"
-      />
+      <div
+        class="w-full h-52 rounded-3xl bg-gray-100 "
+      ></div>
+      <!-- Image 4 -->
+      <div
+        class="w-full h-52 rounded-3xl bg-gray-100 "
+      ></div>
     </div>
   </section>
   <!-- Life in the UK component -->
-  <div class="pt-5 h-screen">
-    <h1 class="text-center text-3xl">Life in the UK</h1>
-  </div>
+  <section class="py-10 max-w-6xl mx-auto">
+    <div class="mx-auto px-4 sm:px-8">
+      <div class="py-8 px-4 mx-auto max-w-4xl text-center lg:py-8 lg:px-0">
+        <h1
+          class="mb-4 text-3xl font-semibold tracking-tight leading-none text-black md:text-4xl lg:text-5xl"
+        >
+          Life in the UK
+        </h1>
+        <p class="mb-0 text-sm font-normal text-gray-900 lg:text-xl sm:px-16">
+          Upon arrival You are collected by the Sponsor from the arrival airport
+          to commence work immediately. Each approved applicant will be given
+          the contact number of Seasonal Jobs for emergency use should
+          circumstances require it. UK agencies will offer accommodation at
+          government rate The government appointed UK agencies will cover all
+          successful applicants with basic medical cover insurance.
+        </p>
+      </div>
+    </div>
+    <!-- Images -->
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 py-8 px-4">
+      <!-- Image 1 -->
+      <!-- <img
+        class="w-full h-52 rounded-2xl"
+        src="https://images.unsplash.com/photo-1550788919-a18fe7099302?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YnJpdGFpbnxlbnwwfHwwfHx8MA%3D%3D"
+        alt="Image Description"
+      /> -->
+      <div
+        class="w-full h-52 rounded-3xl bg-gray-100 "
+      ></div>
+      <!-- Image 2 -->
+      <div
+        class="w-full h-52 rounded-3xl bg-gray-100 "
+      ></div>
+      <!-- Image 3 -->
+      <div
+        class="w-full h-52 rounded-3xl bg-gray-100 "
+      ></div>
+      <!-- Image 4 -->
+      <div
+        class="w-full h-52 rounded-3xl bg-gray-100 "
+      ></div>
+    </div>
+  </section>
   <!-- Farm Life -->
-  <div class="h-screen"><h1 class="text-center text-3xl">Farm Life</h1></div>
+  <section class="py-10 max-w-6xl mx-auto">
+    <div class="mx-auto px-4 sm:px-8">
+      <div class="py-8 px-4 mx-auto max-w-4xl text-center lg:py-8 lg:px-0">
+        <h1
+          class="mb-4 text-3xl font-semibold tracking-tight leading-none text-black md:text-4xl lg:text-5xl"
+        >
+        Farm Life
+        </h1>
+        <p class="mb-0 text-sm font-normal text-gray-900 lg:text-xl sm:px-16">
+          Upon arrival you are collected by the Sponsor from the arrival airport
+          to commence work immediately. Each approved applicant will be given
+          the contact number of Seasonal Jobs for emergency use should
+          circumstances require it. UK agencies will offer accommodation at
+          government rate The government appointed UK agencies will cover all
+          successful applicants with basic medical cover insurance.
+        </p>
+      </div>
+    </div>
+    <!-- Images -->
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 py-8 px-4">
+      <!-- Image 1 -->
+      <!-- <img
+        class="w-full h-52 rounded-2xl"
+        src="https://images.unsplash.com/photo-1550788919-a18fe7099302?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YnJpdGFpbnxlbnwwfHwwfHx8MA%3D%3D"
+        alt="Image Description"
+      /> -->
+      <div
+        class="w-full h-52 rounded-3xl bg-gray-100 "
+      ></div>
+      <!-- Image 2 -->
+      <div
+        class="w-full h-52 rounded-3xl bg-gray-100 "
+      ></div>
+      <!-- Image 3 -->
+      <div
+        class="w-full h-52 rounded-3xl bg-gray-100 "
+      ></div>
+      <!-- Image 4 -->
+      <div
+        class="w-full h-52 rounded-3xl bg-gray-100 "
+      ></div>
+    </div>
+  </section>
 </template>
 
 <script setup></script>
