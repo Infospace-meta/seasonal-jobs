f59e34e74502df2d862a37681ed182af4925b77a
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-05-15 12:02:54 +0300

    message

--- app/src/components/FindMoreComponent.vue
+++ app/src/components/FindMoreComponent.vue
@@ -1,9 +1,53 @@
 <template>
-  <div class="pt-5 h-screen"><h1 class="text-center text-3xl">Life in the UK</h1></div>
+  <!-- Visit and Travel -->
+  <section class="py-20 max-w-6xl mx-auto">
+    <div class="mx-auto px-4 sm:px-8">
+      <div class="py-8 px-4 mx-auto max-w-4xl text-center lg:py-8 lg:px-0">
+        <h1
+          class="mb-4 text-2xl font-semibold tracking-tight leading-none text-black md:text-3xl lg:text-4xl"
+        >
+          Traveling to the UK
+        </h1>
+        <p class="mb-0 text-sm font-normal text-gray-900 lg:text-xl sm:px-16">
+          After application what next? <br/> 
+          It is the responsibility of the applicant
+          to pay for the Biometric data collection & the Seasonal Job Visa which
+          will be approved with a Sponsorship Number from the UK. then obtain a
+          passport, a letter of Good Conduct (confirm necessity), Visa with
+          biometrics and, finally, with the assistance of Seasonal Jobs an air
+          ticket for travel.
+        </p>
+      </div>
+    </div>
+    <!-- Images -->
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 py-8 px-4">
+      <!-- Image 1 -->
+      <img
+        class="w-full h-52 rounded-2xl"
+        src="https://images.unsplash.com/photo-1550788919-a18fe7099302?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YnJpdGFpbnxlbnwwfHwwfHx8MA%3D%3D"
+        alt="Image Description"
+      />
+      <!-- Image 2 -->
+      <img
+        class="w-full h-52 rounded-2xl"
+        src="https://images.unsplash.com/photo-1444858291040-58f756a3bdd6?q=80&w=1978&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Image Description"
+      />
+      <!-- Image 3 -->
 
+      <img
+        class="w-full h-52 rounded-2xl"
+        src="https://images.unsplash.com/photo-1436491865332-7a61a109cc05?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Image Description"
+      />
+    </div>
+  </section>
+  <!-- Life in the UK component -->
+  <div class="pt-5 h-screen">
+    <h1 class="text-center text-3xl">Life in the UK</h1>
+  </div>
+  <!-- Farm Life -->
   <div class="h-screen"><h1 class="text-center text-3xl">Farm Life</h1></div>
-
-  <div class="h-screen"><h1 class="text-center text-3xl">Visit and Travel</h1></div>
 </template>
 
 <script setup></script>
