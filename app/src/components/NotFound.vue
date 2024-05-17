f9eae144a45df1be06e2633e02fcf91c91c1b2f5
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-05-17 15:41:26 +0300

    message

---
+++ app/src/components/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="py-10 px-4 sm:px-6 lg:px-8 text-center space-y-6">
+      <h1 class="block text-7xl font-bold text-gray-800 sm:text-9xl">404</h1>
+      <p class="mt-3 text-gray-600 text-3xl">Oops, something went wrong.</p>
+      <p class="text-gray-600 text-xl">Sorry, we didn't find the page you were looking for.</p>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ app/src/views/NotFoundView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-white h-96 flex flex-col justify-center items-center">
+    <NotFound />
+  </div>
+</template>
+
+<script setup>
+import NotFound from "../components/NotFound.vue";
+</script>
+
+<style lang="scss" scoped></style>
