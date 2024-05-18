5deaa883811e2da7b146d274700f925146b56b6e
meccah <georgekimiti79@gmail.com>
2024-05-18 07:40:14 +0300

    message

--- app/src/components/PrivacyPolicy.vue
+++ app/src/components/PrivacyPolicy.vue
@@ -1,94 +1,145 @@
 <template>
-    <div>
-<p>Seasonal Jobs values the privacy of our users. Therefore, this privacy policy explains in detail how we  use and protect the information we collect when you visit our website, as well as our social platforms. Read this privacy policy completely. Please refrain from visiting the site if the terms outlined below are not satisfactory to you.
-We reserve the right to change this policy at any time and will list these changes in the updates section of the policy. By reading this notice and visiting the site, you agree that you understand that customers will not be personally notified when this policy changes.
-Therefore, we advise our customers to frequently review our privacy policy so that they remain aware of its updates. By using the site, you accept that the posted policy and all its changes apply to your interaction with Seasonal Jobs.
-</p>
-<h2>Information Collected by Seasonal Jobs</h2>
-<p>Personal information may be collected by this site in many ways. This information includes:</p> 
-<li>	Personal identifying information like your name, address, email, phone number, age, gender, and other personal data</li>
-<li>	Server data related to the IP address you used to visit our Seasonal Jobs Website, which includes your address, browser, OS, access time, and site activity.</li>
-<li>	Financial information related to your orders including your payment method and identifying payment information. We rarely store financial information collected on our site for transaction purposes. That information gets sent directly to our payment processor, Intasend.</li>
-<li>	Social network data including Facebook permissions and user information from other networks, provided you log onto our site using one of these media sites.</li>
-<li>	Mobile device information such as your device ID, model, and location, if you use our site by accessing it through our mobile app.</li>
+  <section class="max-w-5xl mx-auto">
+    <div class="py-5">
+      <p class="py-2">
+        Seasonal Jobs values the privacy of our users. Therefore, this privacy
+        policy explains in detail how we use and protect the information we
+        collect when you visit our website, as well as our social platforms.
+        Read this privacy policy completely. Please refrain from visiting the
+        site if the terms outlined below are not satisfactory to you. We reserve
+        the right to change this policy at any time and will list these changes
+        in the updates section of the policy. By reading this notice and
+        visiting the site, you agree that you understand that customers will not
+        be personally notified when this policy changes. Therefore, we advise
+        our customers to frequently review our privacy policy so that they
+        remain aware of its updates. By using the site, you accept that the
+        posted policy and all its changes apply to your interaction with
+        Seasonal Jobs.
+      </p>
+      <h2 class="py-2 font-bold">Information Collected by Seasonal Jobs</h2>
+      <p class="py-2">
+        Personal information may be collected by this site in many ways. This
+        information includes:
+      </p>
+      <div class="py-2 list-decimal">
+        <li>
+          Personal identifying information like your name, address, email, phone
+          number, age, gender, and other personal data
+        </li>
+        <li>
+          Server data related to the IP address you used to visit our Seasonal
+          Jobs Website, which includes your address, browser, OS, access time,
+          and site activity.
+        </li>
+        <li>
+          Financial information related to your orders including your payment
+          method and identifying payment information. We rarely store financial
+          information collected on our site for transaction purposes. That
+          information gets sent directly to our payment processor, Intasend.
+        </li>
+        <li>
+          Social network data including Facebook permissions and user
+          information from other networks, provided you log onto our site using
+          one of these media sites.
+        </li>
+        <li>
+          Mobile device information such as your device ID, model, and location,
+          if you use our site by accessing it through our mobile app.
+        </li>
+      </div>
+      <h2 class="py-2 font-bold">How We Use This Information</h2>
+    
+        <p class="py-2">Our Seasonal Jobs Website uses information collected to:</p>
+        <div class="list-decimal py-2">
+        <li>Manage your account information</li>
+        <li>Customize ads</li>
+        <li>Deliver promotions</li>
+        <li>Email your account confirmation</li>
+        <li>Manage purchases and payments</li>
+        <li>Increase site efficiency</li>
+        <li>Notify you of updates</li>
+        <li>Offer new products</li>
+        <li>Monitor and prevent theft</li>
+        <li>Request your customer feedback</li>
+        <li>Resolve account disputes</li>
+        <li>Respond to your service requests</li>
+      </div>
+      <h2 class="py-2 font-bold">Information Disclosure</h2>
+      <p class="py-2">
+        Normally, your information stays on our site. However, below we have
+        listed the situations that may require us to share the information we
+        collect from you:
+      </p >
+      <div class="list-decimal py-2">
+        <li>When required by law, such as for fraud protection</li>
+        <li>
+          With our third-party providers for payment processing and hosting
+        </li>
+        <li>With your consent for marketing purposes</li>
+        <li>When you post comments on the site</li>
+        <li>To our advertisers, affiliates, and partners</li>
+        <li>If this site goes bankrupt and data must be transferred</li>
+      </div>
+      <h2 class="py-2 font-bold">Cookies, Trackers, and Online Ads</h2>
 
-
-<h2>How We Use This Information</h2>
-
-
-
-
-
-<p> Our Seasonal Jobs Website uses information collected to:</p>
-<li>	Manage your account information</li>
-<li>	Customize ads</li>
-<li>	Deliver promotions</li>
-<li>	Email your account confirmation</li>
-<li>	Manage purchases and payments</li>
-<li>	Increase site efficiency</li>
-<li>	Notify you of updates</li>
-<li>	Offer new products</li>
-<li>	Monitor and prevent theft</li>
-<li>	Request your customer feedback</li>
-<li>	Resolve account disputes</li>
-<li>	Respond to your service requests</li>
-
-
-
-
-
-
-
-
-
-
-
-
-<h2>Information Disclosure</h2>
-<p>Normally, your information stays on our site. However, below we have listed the situations that may require us to share the information we collect from you:</p>
-
-<li>When required by law, such as for fraud protection</li>
-<li>With our third-party providers for payment processing and hosting</li>
-<li>With your consent for marketing purposes</li>
-<li>When you post comments on the site</li>
-<li>To our advertisers, affiliates, and partners</li>
-<li>If this site goes bankrupt and data must be transferred</li>
-	
-	
-	
-	
-	<h2>Cookies, Trackers, and Online Ads</h2>
-	
-
-
-
-<p> We may use cookies, trackers, web beacons, and other technology to customize our Seasonal Jobs Website to improve your experience. We may customize the site using this information. These trackers do not have access to your personal information and can be removed from your browser options.
-In addition, third-party software provides ads for our site for marketing campaigns. These programs have access to tracking technology to optimize your ad experience. For more information about these ads, visit advert.idonotknow.com
-Website analytics such as through [list vendors and link to their policies] may also be used to track users and remarket our [website/mobile application]. We do not give these vendors access to your personal information.
-</p>
-Other Sites
-Our website may contain links to third-party websites in the form of policies, ads, and other non- affiliated links. Once you leave our site, we are no longer responsible for how your information is collected and disclosed. Please refer to the privacy policies of those third-party sites for more information.
-
-
-Information Security
-We take technical and administrative precautions to protect your data, but we cannot guarantee its safety against all types of fraud or misuse. If you provide personal information, we cannot verify its total security against all types of interception.
-Do-Not-Track
-Some browsers offer Do-Not-Track settings to prevent any information from being distributed. Since these settings have not been legally established as standard practice, we do not acknowledge these settings.
-Additional Options
-At any time, you may opt to review or change your account settings, including contact information. If you wish to delete your account, you may do so to remove most of your information, however, some identifying information will be retained to prevent fraud.
-You may also opt-out of emails and other correspondences from our site at any time.
-Kenya’s Privacy Rights
-Enumerate your state’s privacy codes and any permits, legal age limits, or associated information necessary for people in your specific jurisdiction.
-Contact Us
-If you have questions or concerns about this privacy policy, please feel free to contact us at: Seasonal Jobs, 128 City Road, London, +254 796309986, info@seasonaljobsuk.com.
-
+      <p class="py-2">
+        We may use cookies, trackers, web beacons, and other technology to
+        customize our Seasonal Jobs Website to improve your experience. We may
+        customize the site using this information. These trackers do not have
+        access to your personal information and can be removed from your browser
+        options. In addition, third-party software provides ads for our site for
+        marketing campaigns. These programs have access to tracking technology
+        to optimize your ad experience. For more information about these ads,
+        visit advert.idonotknow.com Website analytics such as through [list
+        vendors and link to their policies] may also be used to track users and
+        remarket our [website/mobile application]. We do not give these vendors
+        access to your personal information.
+      </p>
+      <h2 class="py-2 font-bold">Other Sites</h2>
+      <p class="py-2">
+        Our website may contain links to third-party websites in the form of
+        policies, ads, and other non- affiliated links. Once you leave our site,
+        we are no longer responsible for how your information is collected and
+        disclosed. Please refer to the privacy policies of those third-party
+        sites for more information.
+      </p>
+      <h2 class="py-2 font-bold">Information Security</h2>
+      <p>
+        We take technical and administrative precautions to protect your data,
+        but we cannot guarantee its safety against all types of fraud or misuse.
+        If you provide personal information, we cannot verify its total security
+        against all types of interception.
+      </p>
+      <h2 class="py-2 font-bold">Do-Not-Track</h2>
+      <p class="py-2">
+        Some browsers offer Do-Not-Track settings to prevent any information
+        from being distributed. Since these settings have not been legally
+        established as standard practice, we do not acknowledge these settings.
+      </p>
+      <h2 class="py-2 font-bold">Additional Options</h2>
+      <p class="py-2">
+        At any time, you may opt to review or change your account settings,
+        including contact information. If you wish to delete your account, you
+        may do so to remove most of your information, however, some identifying
+        information will be retained to prevent fraud. You may also opt-out of
+        emails and other correspondences from our site at any time.
+      </p>
+      <h2 class="py-2 font-bold">Kenya’s Privacy Rights</h2>
+      <i class="py-2">
+        >Enumerate your state’s privacy codes and any permits, legal age limits,
+        or associated information necessary for people in your specific
+        jurisdiction.</i
+      >
+      <h2 class="py-2 font-bold">Contact Us</h2>
+      <p class="py-2">
+        If you have questions or concerns about this privacy policy, please feel
+        free to contact us at:<br/> <span class="font-semibold">Seasonal Jobs, 128 City Road, London, +254
+        796309986, info@seasonaljobsuk.com.</span>
+      </p>
     </div>
+  </section>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
