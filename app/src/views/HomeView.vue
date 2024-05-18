afab598a3edefd6b51c79fa2d08d870efc64b421
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-05-18 07:39:51 +0300

    message

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative">
-    <div class="inset-0 bg-white ">
+    <div class="inset-0 bg-white">
       <Hero />
     </div>
 
@@ -8,8 +8,7 @@
       <HomeApplicationProcess />
       <aboutus />
       <HomeCards />
-      <Rememberthis/>
-      <!-- <Calltoaction /> -->
+      <Rememberthis />
     </div>
   </div>
 </template>
@@ -18,8 +17,6 @@
 import Hero from "../components/Hero.vue";
 import HomeApplicationProcess from "../components/HomeApplicationProcess.vue";
 import aboutus from "../components/aboutus.vue";
-import Calltoaction from "../components/calltoaction.vue";
 import HomeCards from "../components/HomeCards.vue";
-import HomeAboutYou from "../components/HomeAboutYou.vue";
 import Rememberthis from "../components/Rememberthis.vue";
 </script>
