064f3a4979ced3ccca8b4637826ba2320786af65
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2024-05-22 01:59:48 +0300

    message

--- app/src/components/HomeApplicationProcess.vue
+++ app/src/components/HomeApplicationProcess.vue
@@ -1,147 +1,93 @@
 <template>
-  <section class="py-10 bg-white sm:py-16 lg:py-24">
-    <div class="px-4 mx-auto max-w-6xl sm:px-6 lg:px-7">
-      <div class="max-w-2xl mx-auto text-center">
-        <h2
-          class="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl"
+  <!-- Cards -->
+  <section class="py-10 bg-green-50 sm:py-16 lg:py-24">
+    <div class="max-w-2xl mx-auto text-center mb-9">
+      <h2
+        class="text-3xl font-bold leading-tight text-black sm:text-4xl lg:text-5xl"
+      >
+        Application Process
+      </h2>
+      <p
+        class="max-w-lg mx-auto mt-10 text-xl leading-relaxed text-gray-800 font-serif"
+      >
+        Only pay <span class="font-bold">£9 (KSH 1490)</span> Change your future
+        for a real opportunity in 2024. Act now next week may be too late and to
+        win it you must be in it
+      </p>
+    </div>
+    <div
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12 max-w-6xl mx-auto"
+    >
+      <!-- Card 1 -->
+      <div class="rounded-2xl px-6 py-4 bg-white">
+        <div
+          class="bg-green-700 flex justify-center items-center mb-8 rounded-xl h-14 w-14"
         >
-          Application Process
-        </h2>
-        <p
-          class="max-w-lg mx-auto mt-10 text-xl leading-relaxed text-gray-600 font-serif"
-        >
-           Only pay <span class="font-bold">£9 (KSH 1490)</span> Change your
-          future for a real opportunity in 2024. Act now next week may be too
-          late and to win it you must be in it
+          <img src="../assets/icons/pen.svg" class="" alt="pen" />
+        </div>
+        <h3 class="mb-3 font-bold text-gray-800 text-xl">Fill in Details</h3>
+        <p class="text-lg text-gray-900">
+          Fill out the form and pay £9 (KSH 1500) fee
         </p>
       </div>
-
-      <div class="relative mt-12 lg:mt-20">
+      <!-- Card 2 -->
+      <div class="rounded-2xl px-6 py-4 bg-white">
         <div
-          class="absolute inset-x-0 hidden xl:px-44 top-2 md:block md:px-20 lg:px-28"
+          class="bg-green-700 h-14 w-14 flex justify-center items-center mb-8 rounded-xl"
         >
-          <img
-            class="w-full"
-            src="../assets/icons/curved-dotted-line.svg"
-            alt=""
-          />
+          <img src="../assets/icons/pen.svg" class="" alt="pen" />
         </div>
-
+        <h3 class="mb-3 font-bold text-gray-800 text-xl">Apply</h3>
+        <p class="text-lg text-gray-900">
+          Receive details of how to apply to UK government approved recruitment
+          agencies.
+        </p>
+      </div>
+      <!-- Card 3 -->
+      <div class="rounded-2xl px-6 py-4 bg-white">
         <div
-          class="relative grid grid-cols-1 text-center gap-y-12 md:grid-cols-3 gap-x-12"
+          class="bg-green-700 h-14 w-14 flex justify-center items-center mb-8 rounded-xl"
         >
-          <div>
-            <div
-              class="flex items-center justify-center w-16 h-16 mx-auto bg-green-700 border-2 border-gray-200 rounded-full shadow"
-            >
-              <span class="text-xl font-semibold text-white"> 1 </span>
-            </div>
-            <h3
-              class="mt-6 text-xl font-semibold leading-tight text-black md:mt-10"
-            >
-              Register
-            </h3>
-            <p class="mt-4 text-base text-gray-600">
-               Fill out the form and pay £9 (KSH 1500) fee
-            </p>
-          </div>
-
-          <div>
-            <div
-              class="flex items-center justify-center w-16 h-16 mx-auto bg-green-700 border-2 border-gray-200 rounded-full shadow"
-            >
-              <span class="text-xl font-semibold text-white"> 2 </span>
-            </div>
-            <h3
-              class="mt-6 text-xl font-semibold leading-tight text-black md:mt-10"
-            >
-              Apply
-            </h3>
-            <p class="mt-4 text-base text-gray-600">
-              Receive details of how to apply to UK government approved recruitment agencies.
-            </p>
-          </div>
-
-          <div>
-            <div
-              class="flex items-center justify-center w-16 h-16 mx-auto bg-green-700 border-2 border-gray-200 rounded-full shadow"
-            >
-              <span class="text-xl font-semibold text-white"> 3 </span>
-            </div>
-            <h3
-              class="mt-6 text-xl font-semibold leading-tight text-black md:mt-10"
-            >
-              Receive
-            </h3>
-            <p class="mt-4 text-base text-gray-600">
-               Receive an email with the outcome of your application.
-            </p>
-          </div>
+          <img src="../assets/icons/pen.svg" class="" alt="pen" />
         </div>
-
-        <div class="relative mt-12 lg:mt-24">
+        <h3 class="mb-3 font-bold text-gray-800 text-xl">Receive</h3>
+        <p class="text-lg text-gray-900">
+          Receive an email with the outcome of your application.
+        </p>
+      </div>
+      <!-- Card 4 -->
+      <div class="rounded-2xl px-6 py-4 bg-white">
         <div
-          class="absolute inset-x-0 hidden xl:px-44 top-2 md:block md:px-20 lg:px-28"
+          class="bg-green-700 h-14 w-14 flex justify-center items-center mb-8 rounded-xl"
         >
-          <img
-            class="w-full"
-            src="../assets/icons/curved-dotted-line.svg"
-            alt=""
-          />
+          <img src="../assets/icons/pen.svg" class="" alt="pen" />
         </div>
-
+        <h3 class="mb-3 font-bold text-gray-800 text-xl">Travel</h3>
+        <p class="text-lg text-gray-900">Travel abroad to the UK</p>
+      </div>
+      <!-- Card 5 -->
+      <div class="rounded-2xl px-6 py-4 bg-white">
         <div
-          class="relative grid grid-cols-1 text-center gap-y-12 md:grid-cols-3 gap-x-12"
+          class="bg-green-700 h-14 w-14 flex justify-center items-center mb-8 rounded-xl"
         >
-          <div>
-            <div
-              class="flex items-center justify-center w-16 h-16 mx-auto bg-green-700 border-2 border-gray-200 rounded-full shadow"
-            >
-              <span class="text-xl font-semibold text-white"> 4 </span>
-            </div>
-            <h3
-              class="mt-6 text-xl font-semibold leading-tight text-black md:mt-10"
-            >
-              Travel
-            </h3>
-            <p class="mt-4 text-base text-gray-600">
-             Travel abroad to the UK
-            </p>
-          </div>
-
-          <div>
-            <div
-              class="flex items-center justify-center w-16 h-16 mx-auto bg-green-700 border-2 border-gray-200 rounded-full shadow"
-            >
-              <span class="text-xl font-semibold text-white"> 5 </span>
-            </div>
-            <h3
-              class="mt-6 text-xl font-semibold leading-tight text-black md:mt-10"
-            >
-              Work
-            </h3>
-            <p class="mt-4 text-base text-gray-600">
-             Fruit & vegetable picking & packing plus many other jobs
-            </p>
-          </div>
-
-          <div>
-            <div
-              class="flex items-center justify-center w-16 h-16 mx-auto bg-green-700 border-2 border-gray-200 rounded-full shadow"
-            >
-              <span class="text-xl font-semibold text-white"> 6 </span>
-            </div>
-            <h3
-              class="mt-6 text-xl font-semibold leading-tight text-black md:mt-10"
-            >
-              Bring it Home
-            </h3>
-            <p class="mt-4 text-base text-gray-600">
-              Travel back to Kenya and develop your family and country
-            </p>
-          </div>
+          <img src="../assets/icons/pen.svg" class="" alt="pen" />
         </div>
+        <h3 class="mb-3 font-bold text-gray-800 text-xl">Work</h3>
+        <p class="text-lg text-gray-900">
+          Fruit & vegetable picking & packing plus many other jobs
+        </p>
+      </div>
+      <!-- Card 6 -->
+      <div class="rounded-2xl px-6 py-4 bg-white">
+        <div
+          class="bg-green-700 h-14 w-14 flex justify-center items-center mb-8 rounded-xl"
+        >
+          <img src="../assets/icons/pen.svg" class="" alt="pen" />
         </div>
+        <h3 class="mb-3 font-bold text-gray-800 text-xl">Bring it Home</h3>
+        <p class="text-lg text-gray-900">
+          Travel back to Kenya and develop your family and country
+        </p>
       </div>
     </div>
   </section>
