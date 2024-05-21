8220ad9760b53190d20fa19ba6636d63330d29a1
meccah <georgekimiti79@gmail.com>
2024-05-22 01:10:39 +0300

    messag

--- app/src/components/aboutus.vue
+++ app/src/components/aboutus.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="mx-auto max-w-5xl py-10">
-    <div class="grid grid-cols-1 justify-content-center place-content-center">
+    <div class="grid grid-cols-1 justify-content-center place-content-center bg-[url('./assets/images/pexels.jpg')] bg-cover">
       <div class="text-center py-10 space-y-8">
         <h1 class="text-5xl text-gray-900 font-bold">About Us</h1>
         <p class="text-xl leading-8 tracking-tight px-10 text-gray-600">
@@ -12,13 +12,15 @@
           seasons.
         </p>
       </div>
+
+
       <div
         class="grid grid-cols-1 justify-content-center place-content-center sm:grid sm:grid-cols-2 lg:grid lg:grid-cols-3 gap-10 px-10 py-5"
       >
         <div
-          class="border-[1px] border-gray-300 p-5 rounded-2xl hover:shadow-xl hover:bg-slate-50 bg-orange-100"
+          class="border-[1px] border-gray-300 p-5 rounded-2xl hover:shadow-xl hover:bg-slate-50 bg-slate-800 text-white hover:text-gray-900"
         >
-          <div class="place-self-center">
+          <div class="ml-10 my-5 p-4 ">
             <img
               src="../assets/icons/airplane.svg"
               class="h-12 w-12 rotate-45"
@@ -30,19 +32,21 @@
               to="/about"
               class="flex justify-center items-center space-x-3 py-5 font-serif"
             >
-              <p class="text-xl font-bold text-black">"Seasonal</p>
-              <p class="text-xl font-bold text-black">Jobs"</p>
+              <p class="text-xl font-bold ">"Seasonal</p>
+              <p class="text-xl font-bold ">Jobs"</p>
             </router-link>
           </div>
-          <p class="text-center text-gray-600">
+          <p class="text-center">
             The program is to promote youth participation in Agriculture through
             metorship and skills transfer.
           </p>
         </div>
+        
+
         <div
-          class="border-[1px] border-gray-300 p-5 rounded-2xl hover:shadow-xl hover:bg-slate-50 bg-orange-100"
+          class="border-[1px] border-gray-300 p-5 rounded-2xl hover:shadow-xl hover:bg-slate-50 bg-slate-800 text-white hover:text-gray-900 "
         >
-          <div class="place-self-center">
+          <div class="ml-10 my-5 p-4 ">
             <img
               src="../assets/icons/house-check-fill.svg"
               class="h-12 w-12"
@@ -53,21 +57,22 @@
           <div
             class="flex justify-center items-center space-x-2 font-serif py-5"
           >
-            <p class="text-xl text-black font-bold">"Bring</p>
-            <p class="text-xl text-black font-bold">it</p>
-            <p class="text-xl text-black font-bold">Home!"</p>
+            <p class="text-xl  font-bold">"Bring</p>
+            <p class="text-xl  font-bold">it</p>
+            <p class="text-xl font-bold">Home!"</p>
           </div>
-          <p class="text-center text-gray-600">
+          <p class="text-center ">
             The “Bring it Home!” concept is one that ensures, investments
             brought are back home
            "Kenya" .
           </p>
         </div>
 
+
         <div
-          class="border-[1px] border-gray-300 p-5 rounded-2xl hover:shadow-xl hover:bg-slate-50 bg-orange-100"
+          class="border-[1px] border-gray-300 p-5 rounded-2xl hover:shadow-xl hover:bg-slate-50 bg-slate-800 text-white hover:text-gray-900"
         >
-          <div class="place-self-center">
+          <div class="ml-10 my-5 p-4 ">
             <img
               src="../assets/icons/money-svgrepo-com.svg"
               class="h-12 w-12"
@@ -77,14 +82,16 @@
           <div
             class="flex justify-center items-center space-x-2 font-serif py-5"
           >
-            <p class="text-black text-xl font-semibold">"YouthFund</p>
-            <p class="text-black text-xl font-semibold">Enterprise"</p>
+            <p class=" text-xl font-semibold">"YouthFund</p>
+            <p class=" text-xl font-semibold">Enterprise"</p>
           </div>
-          <p class="text-center text-gray-600">
+          <p class="text-center ">
             Empowering youth through enterprise for economic growth and
             development.
           </p>
         </div>
+
+        
       </div>
     </div>
   </section>
