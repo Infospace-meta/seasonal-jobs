f1115cac0c4af02266deefbddf18237975ac5e8c
immanuel4082 <immanuel4082@gmail.com>
2024-10-31 01:15:47 +0300

    message

--- app/src/components/contact.vue
+++ app/src/components/contact.vue
@@ -15,7 +15,7 @@
                     <a href="#"
                         class="transition-all duration-1000 bg-white hover:bg-black border hover:shadow-xl m-2 p-4 relative group">
                         <div
-                            class="absolute bg-black/50 top-0 left-0 w-24 h-1 transition-all duration-200 group-hover:bg-black group-hover:w-1/2">
+                            class="absolute bg-black/50 top-0 left-0 w-24 h-1 transition-all duration-200 group-hover:bg-white group-hover:w-1/2">
                         </div>
                         <div class="py-2 px-9">
 
@@ -34,17 +34,17 @@
                     <a href="#"
                         class="transition-all duration-1000 bg-white hover:bg-red-600 border hover:shadow-xl m-2 p-4 relative group  ">
                         <div
-                            class=" absolute  bg-black/50 top-0 left-0 w-24 h-1 transition-all duration-200 group-hover:bg-black group-hover:w-1/2  ">
+                            class=" absolute  bg-red-600 top-0 left-0 w-24 h-1 transition-all duration-200 group-hover:bg-white group-hover:w-1/2  ">
                         </div>
                         <div class="py-2 px-9 relative  ">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 h-16 group-hover:fill-white">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-16 h-16 fill-red-600 group-hover:fill-white">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
                         </svg>
 
 
-                            <h3 class="mt-8 text-3xl font-semibold text-black group-hover:text-white">Phone
+                            <h3 class="mt-8 text-3xl font-semibold text-red-600 group-hover:text-white">Phone
                             </h3>
-                            <p class="mt-4 text-base text-black group-hover:text-white">+25496309956<br>
+                            <p class="mt-4 text-base text-red-600 group-hover:text-white">+25496309956<br>
                                 +254787654321
                             </p>
                         </div>
@@ -53,17 +53,17 @@
                     <a href="#"
                         class="transition-all duration-1000 bg-white hover:bg-green-700 border hover:shadow-xl m-2 p-4 relative group  ">
                         <div
-                            class=" absolute  bg-black/50 top-0 left-0 w-24 h-1 transition-all duration-200 group-hover:bg-black group-hover:w-1/2  ">
+                            class="absolute bg-green-700 top-0 left-0 w-24 h-1 transition-all duration-200 group-hover:bg-white group-hover:w-1/2  ">
                         </div>
                         <div class="py-2 px-9 relative">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-16 h-16">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="rgb(21 128 61)" class="w-16 h-16">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
                         </svg>
 
-                            <h3 class="mt-8 text-3xl font-semibold text-black group-hover:text-white">Location
+                            <h3 class="mt-8 text-3xl font-semibold text-green-700 group-hover:text-white">Location
                         </h3>
-                            <p class="mt-4 text-base text-black group-hover:text-white">United Kingdom<br>
+                            <p class="mt-4 text-base text-green-700 group-hover:text-white">United Kingdom<br>
                                 City Road,
                                 London</p>
                         </div>
